9ca2966e00217b0f274b65b416a159a50c4fc74f
fxn304 <floriann.neacsu@yahoo.com>
2022-01-26 00:17:05 +0200

    aa

--- starter-code/assets/scripts/destination-pages.js
+++ starter-code/assets/scripts/destination-pages.js
@@ -24,7 +24,7 @@ const europa = {
   time: "3 years"
 };
 const titan = {
-  id: 3,
+  id: 4,
   image: "./assets/destination/image-titan.webp",
   title: "Titan",
   text: "The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.",
